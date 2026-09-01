bff6729954b93d0311ee6b7cafd0e6b0068a2d9b
Rodrigo5431 <rodrigokarvalho12890@gmail.com>
2026-08-31 21:17:59 -0300

    organization

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -8,7 +8,7 @@ import { nitro } from "nitro/vite";
 export default defineConfig({
   plugins: [
     tanstackStart(), 
-    nitro({ preset: 'vercel' }),
+    nitro({ preset: 'netlify' }), 
     viteReact(),
     tailwindcss(),
   ],
